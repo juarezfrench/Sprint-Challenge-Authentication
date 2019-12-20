2efc89f6c9012d4a1352ed7c2f8ec79c26f7f6b3
juarezfrench <markedfrench@gmail.com>
2019-12-20 12:17:29 -0600

    Finished authenticate middleware, auth-router

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,35 +1,21 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
-
-const bcrypt = require('bcryptjs');
-
-// const Users = require('../users/users-model.js');
+const jwt = require("jsonwebtoken"); // installed this
 
 module.exports = (req, res, next) => {
-  const {authorization} = req.headers
-  jwt.encode(secret, payload, function (err, token) {
-    if (err) {
-      console.error(err.name, err.message);
-    } else {
-      console.log(token);
+  const { authorization } = req.headers
 
-  if (username && password) {
-    Users.findBy({ username }
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: 'Invalid Credentials' });
-        }
-      })
-      .catch(error => {
-        res.status(500).json({ message: 'Ran into an unexpected error' });
-      }));
+  if (authorization) {
+    const secret = process.env.JWT_SECRET || "is it secret, is it safe?";
+
+    jwt.verify(authorization, secret, function(err, decodedToken) {
+      if (err) {
+        res.status(401).json({ message: "Invalid Token" });
+      } else {
+        req.token = decodedToken;
+
+        next();
+      }
+    });
   } else {
-    res.status(400).json({ message: 'No credentials provided' });
+    res.status(400).json({ message: "Please login and try again" });
   }
-}}
-  )};
+};
