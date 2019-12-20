cb0723978eaf17c2bdfff3b7a71ed58b7e75aa59
juarezfrench <markedfrench@gmail.com>
2019-12-20 12:09:21 -0600

    Set up, authenticate-middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,33 @@
   before granting access to the next middleware/route handler
 */
 
+const bcrypt = require('bcryptjs');
+
+// const Users = require('../users/users-model.js');
+
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
-};
+  const {authorization} = req.headers
+  jwt.encode(secret, payload, function (err, token) {
+    if (err) {
+      console.error(err.name, err.message);
+    } else {
+      console.log(token);
+
+  if (username && password) {
+    Users.findBy({ username }
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          next();
+        } else {
+          res.status(401).json({ message: 'Invalid Credentials' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json({ message: 'Ran into an unexpected error' });
+      }));
+  } else {
+    res.status(400).json({ message: 'No credentials provided' });
+  }
+}}
+  )};
